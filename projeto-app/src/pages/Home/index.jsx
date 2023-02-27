303f79abb90bee4f1d7e7d6f23adcf1e82ea55cb
rewerp <rewer_p@hotmail.com>
2023-02-27 20:18:44 -0300

    Adicionado o inicio do front e componentes PageFooter, Header, Home

---
+++ projeto-app/src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Header } from "../../components/Header";
+import { PageFooter } from "../../components/PageFooter";
+
+export function Home() {
+  return (
+    <div>
+      <Header />
+      <PageFooter />
+    </div>
+  );
+}
